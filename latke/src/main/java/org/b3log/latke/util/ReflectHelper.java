ac1131d6cce008b67fbfa8d5d32fd15ce276884c
yaoliceng <yaoliceng@yaoliceng-Vostro-3350>
2012-09-10 22:39:32 +0800

    反射util  利用class字节码 读class信息  需要学习javasist用法  重写这个类 现在不支持重载判断

--- latke/src/main/java/org/b3log/latke/util/ReflectHelper.java
+++ latke/src/main/java/org/b3log/latke/util/ReflectHelper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2009, 2010, 2011, 2012, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.b3log.latke.util;
 
 import javassist.ClassClassPath;
@@ -10,34 +25,52 @@ import javassist.bytecode.CodeAttribute;
 import javassist.bytecode.LocalVariableAttribute;
 import javassist.bytecode.MethodInfo;
 
-public class ReflectHelper {
-	
-	public static String[] getMethodVariableNames(Class<?> clazz, String targetMethodName) {  
-	 
-	    ClassPool pool = ClassPool.getDefault();  
-	    pool.insertClassPath(new ClassClassPath(clazz));  
-	    CtClass cc;  
-	    CtMethod cm = null;  
-	    try {  
-	        cc = pool.get(clazz.getName());  
-	        cm = cc.getDeclaredMethod(targetMethodName);
-	    } catch (NotFoundException e) {  
-	        e.printStackTrace();  
-	    }  
-	  
-	    MethodInfo methodInfo = cm.getMethodInfo();  
-	    CodeAttribute codeAttribute = methodInfo.getCodeAttribute();  
-	    LocalVariableAttribute attr = (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag);  
-	    String[] variableNames = new String[0];  
-	    try {  
-	        variableNames = new String[cm.getParameterTypes().length];  
-	    } catch (NotFoundException e) {  
-	        e.printStackTrace();  
-	    }  
-	    int staticIndex = Modifier.isStatic(cm.getModifiers()) ? 0 : 1;  
-	    for (int i = 0; i < variableNames.length; i++)  
-	        variableNames[i] = attr.variableName(i + staticIndex);  
-	    return variableNames;  
-	}  
+/**
+ * ReflectHelper while not using java reflect instead of the other class byte tool.
+ * @author <a href="mailto:wmainlove@gmail.com">Love Yao</a>
+ * @version 1.0.0.1, Sep 10, 2012
+ */
+public final class ReflectHelper {
+
+    /**
+     * the defalut constructor.
+     */
+    private ReflectHelper() {
+    }
+
+    /**
+     * getMethodVariableNames in user defined.
+     * @param clazz the specific clazz
+     * @param targetMethodName the targetMethodName
+     * @return the String[] of names
+     */
+    public static String[] getMethodVariableNames(final Class<?> clazz, final String targetMethodName) {
+
+        final ClassPool pool = ClassPool.getDefault();
+        pool.insertClassPath(new ClassClassPath(clazz));
+        CtClass cc;
+        CtMethod cm = null;
+        try {
+            cc = pool.get(clazz.getName());
+            cm = cc.getDeclaredMethod(targetMethodName);
+        } catch (final NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final MethodInfo methodInfo = cm.getMethodInfo();
+        final CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
+        final LocalVariableAttribute attr = (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag);
+        String[] variableNames = new String[0];
+        try {
+            variableNames = new String[cm.getParameterTypes().length];
+        } catch (final NotFoundException e) {
+            e.printStackTrace();
+        }
+        final int staticIndex = Modifier.isStatic(cm.getModifiers()) ? 0 : 1;
+        for (int i = 0; i < variableNames.length; i++) {
+            variableNames[i] = attr.variableName(i + staticIndex);
+        }
+        return variableNames;
+    }
 
 }
