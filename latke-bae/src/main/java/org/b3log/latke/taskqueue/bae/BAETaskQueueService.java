d297859965ce9f54f27881f09351dea86904d688
Liang Ding <DL88250@gmail.com>
2012-09-20 17:25:49 +0800

    c

--- latke-bae/src/main/java/org/b3log/latke/taskqueue/bae/BAETaskQueueService.java
+++ latke-bae/src/main/java/org/b3log/latke/taskqueue/bae/BAETaskQueueService.java
@@ -19,7 +19,6 @@ import com.baidu.bae.api.factory.BaeFactory;
 import com.baidu.bae.api.taskqueue.BaeTaskQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.b3log.latke.servlet.HTTPRequestMethod;
 import org.b3log.latke.taskqueue.Queue;
 import org.b3log.latke.taskqueue.Task;
 import org.b3log.latke.taskqueue.TaskHandle;
