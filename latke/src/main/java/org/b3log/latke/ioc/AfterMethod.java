d6d4516cd5630037206000aa6c483491bd9a622c
Liang Ding <d@b3log.org>
2018-12-02 16:50:41 +0800

    :recycle: 拦截器注解移动包

--- latke/src/main/java/org/b3log/latke/ioc/AfterMethod.java
+++ latke/src/main/java/org/b3log/latke/ioc/AfterMethod.java
@@ -13,58 +13,56 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.b3log.latke.intercept.annotation;
-
+package org.b3log.latke.ioc;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-
 /**
- * Indicates that the annotated method will be invoked before a method invocation.
- * 
+ * Indicates that the annotated method will be invoked after a method invocation.
+ *
  * <p>
- * For example, 
- * 
+ * For example,
+ *
  * <pre>
  * package org.b3log;
- * 
+ *
  * publlic class A {
  *     // invoking method
- *     public void oneMethod() {} 
- * 
+ *     public void oneMethod() {}
+ *
  *     // intercept method
- *     &#64;BeforeMethod("oneMethod")
- *     public void b() {} 
+ *     &#64;AfterMethod("oneMethod")
+ *     public void b() {}
  * }
- * 
- * 
+ *
+ *
  * package org.b3log;
- * 
+ *
  * public class B {
  *     // intercept method
- *     &#64;BeforeMethod("org.b3log.A#oneMethod")
+ *     &#64;AfterMethod("org.b3log.A#oneMethod")
  *     public void c() {}
  * }
  * </pre>
- * 
- * Then the method org.b3log.A#b and org.b3log.B#c will be invoked before org.b3log.A#oneMethod, and the invocation of methods A#b and B#c
- * are with uncertain order. In addition, method annotated with this annotation has no transitivity, means that a method (e.g. oneMethod) 
- * be invoked in application code maybe trigger it's before/after method (if them exists), others before/after methods (if them exists) of 
+ * <p>
+ * Then the method org.b3log.A#b and org.b3log.B#c will be invoked after org.b3log.A#oneMethod, and the invocation of methods A#b and B#c
+ * are with uncertain order. In addition, method annotated with this annotation has no transitivity, means that a method (e.g. oneMethod)
+ * be invoked in application code maybe trigger it's before/after method (if them exists), others before/after methods (if them exists) of
  * theses before/after methods of the method (oneMethod) will not be invoked.
  * </p>
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @version 1.0.0.8, Sep 29, 2013
- * @see AfterMethod
+ * @see BeforeMethod
  */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target(java.lang.annotation.ElementType.METHOD)
-public @interface BeforeMethod {
+public @interface AfterMethod {
 
     /**
      * The name of invoking method.
-     * 
+     *
      * @return value
      */
     String value();
