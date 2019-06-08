0052fa960e8552ccc5520798d8967e110c155fcb
Liang Ding <d@b3log.org>
2019-06-08 20:26:02 +0800

    :bookmark: 发布 v2.4.58

--- latke-core/src/main/java/org/b3log/latke/Latkes.java
+++ latke-core/src/main/java/org/b3log/latke/Latkes.java
@@ -66,7 +66,7 @@ public final class Latkes {
     /**
      * Version.
      */
-    public static final String VERSION = "2.4.57";
+    public static final String VERSION = "2.4.58";
 
     /**
      * Application startup time millisecond.
