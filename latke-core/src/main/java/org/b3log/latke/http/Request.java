0e935387ea4f0b5143723b4f19b2c9db2814a99e
Liang Ding <d@b3log.org>
2019-11-07 11:50:48 +0800

    :art: 升级框架

--- latke-core/src/main/java/org/b3log/latke/http/Request.java
+++ latke-core/src/main/java/org/b3log/latke/http/Request.java
@@ -212,7 +212,7 @@ public class Request {
 
     void parseForm(final FullHttpRequest fullHttpRequest) {
         content = (fullHttpRequest.content().toString(CharsetUtil.UTF_8));
-        if (StringUtils.startsWithIgnoreCase(content, "%7B%22")) {
+        if (StringUtils.startsWithIgnoreCase(content, "%7B%22") || StringUtils.startsWithIgnoreCase(content, "{\"")) {
             json = new JSONObject(URLs.decode(content));
         } else {
             final QueryStringDecoder queryDecoder = new QueryStringDecoder(content, false);
