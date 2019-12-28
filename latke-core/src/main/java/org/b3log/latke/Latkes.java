172dc4419ed614e848d132961024376599c1729d
Liang Ding <d@b3log.org>
2019-12-28 18:14:28 +0800

    :bookmark: 发布 v3.0.21

--- latke-core/src/main/java/org/b3log/latke/Latkes.java
+++ latke-core/src/main/java/org/b3log/latke/Latkes.java
@@ -40,7 +40,7 @@ import java.util.concurrent.Executors;
  * Latke framework configuration utility facade.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 2.11.0.13, Dec 9, 2019
+ * @version 2.11.0.14, Dec 28, 2019
  * @see #init()
  * @see #shutdown()
  * @see #getServePath()
@@ -61,7 +61,7 @@ public final class Latkes {
     /**
      * Version.
      */
-    public static final String VERSION = "3.0.20";
+    public static final String VERSION = "3.0.21";
 
     /**
      * Application startup time millisecond.
