8886a2ede461e7fa2cbae8a118b23a641c47861f
Liang Ding <d@b3log.org>
2020-04-09 11:29:23 +0800

    :page_facing_up: 更换开源协议为 木兰宽松许可证, 第2版 Fix #15

--- latke-core/src/main/java/org/b3log/latke/util/AntPathMatcher.java
+++ latke-core/src/main/java/org/b3log/latke/util/AntPathMatcher.java
@@ -1,4 +1,15 @@
 /*
+ * Latke - 一款以 JSON 为主的 Java Web 框架
+ * Copyright (c) 2009-present, b3log.org
+ *
+ * LianDi is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *         http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+/*
  * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
