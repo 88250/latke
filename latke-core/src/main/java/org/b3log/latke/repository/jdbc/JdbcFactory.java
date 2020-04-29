6792f7d7267d24f75c75084a71f98ab6336afca6
Wu Yuncheng <wyunchn@gmail.com>
2020-04-29 16:51:46 +0800

    fix: volatile in double-checked locking

--- latke-core/src/main/java/org/b3log/latke/repository/jdbc/JdbcFactory.java
+++ latke-core/src/main/java/org/b3log/latke/repository/jdbc/JdbcFactory.java
@@ -43,7 +43,7 @@ public final class JdbcFactory implements JdbcDatabase {
     /**
      * the singleton of jdbcfactory.
      */
-    private static JdbcFactory jdbcFactory;
+    private static volatile JdbcFactory jdbcFactory;
 
     /**
      * All JdbcDatabaseSolution class names.
