e1d67d475fa3685f4778557836e0b76ea19f7d5a
Liang Ding <d@b3log.org>
2020-05-13 16:51:23 +0800

    :art: Format code

--- latke-core/src/main/java/org/b3log/latke/repository/jdbc/util/JdbcUtil.java
+++ latke-core/src/main/java/org/b3log/latke/repository/jdbc/util/JdbcUtil.java
@@ -36,7 +36,7 @@ import java.util.Map;
  *
  * @author <a href="https://hacpai.com/member/mainlove">Love Yao</a>
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.2.8, May 17, 2019
+ * @version 1.1.2.9, May 13, 2020
  */
 public final class JdbcUtil {
 
@@ -60,10 +60,9 @@ public final class JdbcUtil {
         }
 
         final Statement statement = connection.createStatement();
-        final boolean isSuccess = !statement.execute(sql);
+        final boolean ret = !statement.execute(sql);
         statement.close();
-
-        return isSuccess;
+        return ret;
     }
 
     /**
@@ -124,7 +123,6 @@ public final class JdbcUtil {
     public static JSONArray queryJsonArray(final String sql, final List<Object> paramList, final Connection connection,
                                            final String tableName, final boolean isDebug) throws SQLException, JSONException, RepositoryException {
         final JSONObject jsonObject = queryJson(sql, paramList, connection, false, tableName, isDebug);
-
         return jsonObject.getJSONArray(Keys.RESULTS);
     }
 
@@ -147,18 +145,14 @@ public final class JdbcUtil {
         }
 
         final PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
         for (int i = 1; i <= paramList.size(); i++) {
             preparedStatement.setObject(i, paramList.get(i - 1));
         }
-
         final ResultSet resultSet = preparedStatement.executeQuery();
-        final JSONObject jsonObject = resultSetToJsonObject(resultSet, ifOnlyOne, tableName);
-
+        final JSONObject ret = resultSetToJsonObject(resultSet, ifOnlyOne, tableName);
         resultSet.close();
         preparedStatement.close();
-
-        return jsonObject;
+        return ret;
     }
 
     /**
@@ -192,14 +186,11 @@ public final class JdbcUtil {
         }
 
         final int numColumns = resultSetMetaData.getColumnCount();
-
         final JSONArray jsonArray = new JSONArray();
         JSONObject jsonObject;
         String columnName;
-
         while (resultSet.next()) {
             jsonObject = new JSONObject();
-
             for (int i = 1; i < numColumns + 1; i++) {
                 columnName = resultSetMetaData.getColumnLabel(i);
                 final FieldDefinition definition = dMap.get(columnName);
@@ -251,7 +242,6 @@ public final class JdbcUtil {
 
         jsonObject = new JSONObject();
         jsonObject.put(Keys.RESULTS, jsonArray);
-
         return jsonObject;
     }
 
