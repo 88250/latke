b02babe108127d46e879e89ae4826836984380f6
Liang Ding <d@b3log.org>
2021-03-28 00:47:36 +0800

    :bookmark: 发布 v3.4.12

--- latke-core/src/main/java/org/b3log/latke/Latkes.java
+++ latke-core/src/main/java/org/b3log/latke/Latkes.java
@@ -57,7 +57,7 @@ public final class Latkes {
     /**
      * Version.
      */
-    public static final String VERSION = "3.4.11";
+    public static final String VERSION = "3.4.12";
 
     /**
      * Application startup time millisecond.
