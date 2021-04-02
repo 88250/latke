05b56c03d732117f0107e0a6cbdcb581ce3a9f75
Liang Ding <d@b3log.org>
2021-04-02 20:12:43 +0800

    :art: 改进 remoteAddr IP 获取

--- latke-core/src/main/java/org/b3log/latke/util/Requests.java
+++ latke-core/src/main/java/org/b3log/latke/util/Requests.java
@@ -24,7 +24,7 @@ import java.util.Iterator;
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author <a href="https://ld246.com/member/e">Dongxu Wang</a>
- * @version 3.1.0.0, May 13, 2020
+ * @version 3.1.0.1, Apr 2, 2021
  */
 public final class Requests {
 
@@ -50,7 +50,6 @@ public final class Requests {
         if (!logger.isEnabled(level)) {
             return;
         }
-
         logger.log(level, getLog(request));
     }
 
@@ -67,7 +66,6 @@ public final class Requests {
 
         final String indents = "    ";
         final StringBuilder logBuilder = new StringBuilder("Request [").append(Strings.LINE_SEPARATOR);
-
         logBuilder.append(indents).append("method=").append(request.getMethod()).append(",").append(Strings.LINE_SEPARATOR);
         logBuilder.append(indents).append("URI=").append(request.getRequestURI()).append(",").append(Strings.LINE_SEPARATOR);
         logBuilder.append(indents).append("contentType=").append(request.getContentType()).append(",").append(Strings.LINE_SEPARATOR);
@@ -85,24 +83,32 @@ public final class Requests {
         headerLogBuilder.append(indents);
         headerLogBuilder.append("]");
         logBuilder.append(headerLogBuilder.toString());
-
         logBuilder.append(Strings.LINE_SEPARATOR).append("]");
-
         return logBuilder.toString();
     }
 
     /**
      * Gets the Internet Protocol (IP) address of the end-client that sent the specified request.
      * <p>
-     * It will try to get HTTP head "X-forwarded-for" or "X-Real-IP" from the last proxy to get the request first, if not found, try to get
-     * it directly by {@link Request#getRemoteAddr()}.
+     * It will try to get HTTP header "Ali-CDN-Real-IP", "CF-Connecting-IP", "X-forwarded-for" and "X-Real-IP" from the
+     * last proxy to get the request first, if not found, try to get it directly by {@link Request#getRemoteAddr()}.
      * </p>
      *
      * @param request the specified request
      * @return the IP address of the end-client sent the specified request
      */
     public static String getRemoteAddr(final Request request) {
-        String ret = request.getHeader("X-Forwarded-For");
+        String ret = request.getHeader("Ali-CDN-Real-IP");
+        if (StringUtils.isNotBlank(ret)) {
+            return ret;
+        }
+
+        ret = request.getHeader("CF-Connecting-IP");
+        if (StringUtils.isNotBlank(ret)) {
+            return ret;
+        }
+
+        ret = request.getHeader("X-Forwarded-For");
         if (StringUtils.isBlank(ret)) {
             ret = request.getHeader("X-Real-IP");
             if (StringUtils.isBlank(ret)) {
@@ -110,15 +116,14 @@ public final class Requests {
             }
         }
 
-        return ret.split(",")[0];
+        final String[] parts = StringUtils.split(ret, ",");
+        ret = parts[parts.length - 1];
+        ret = StringUtils.trim(ret);
+        return ret;
     }
 
     /**
      * Gets the Internet Protocol (IP) address of the end-client that sent the specified request.
-     * <p>
-     * It will try to get HTTP head "X-forwarded-for" or "X-Real-IP" from the last proxy to get the request first, if not found, try to get
-     * it directly by {@link Request#getRemoteAddr()}.
-     * </p>
      *
      * @param context the specified request context
      * @return the IP address of the end-client sent the specified request
@@ -142,7 +147,6 @@ public final class Requests {
                 return request.getScheme();
             }
         }
-
         return ret;
     }
 
@@ -157,7 +161,6 @@ public final class Requests {
         if (StringUtils.isBlank(ret)) {
             return request.getServerName();
         }
-
         return ret;
     }
 
