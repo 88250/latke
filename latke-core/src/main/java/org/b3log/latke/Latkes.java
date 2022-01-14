733da1ccc822ca8fc8392ac8aca809a4455e6ff9
Liang Ding <d@b3log.org>
2022-01-14 11:37:05 +0800

    :arrow_up: 升级依赖

--- latke-core/src/main/java/org/b3log/latke/Latkes.java
+++ latke-core/src/main/java/org/b3log/latke/Latkes.java
@@ -57,7 +57,7 @@ public final class Latkes {
     /**
      * Version.
      */
-    public static final String VERSION = "3.4.21";
+    public static final String VERSION = "3.4.22";
 
     /**
      * Application startup time millisecond.
